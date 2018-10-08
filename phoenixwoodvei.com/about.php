a01e413ad1faa49bd4bf305f15ad3a05af4a5ccb
JeyrikPaduga <35312105+JeyrikPaduga@users.noreply.github.com>
2018-10-08 13:15:23 -0700

    Update about.php

--- phoenixwoodvei.com/about.php
+++ phoenixwoodvei.com/about.php
@@ -22,12 +22,16 @@
 
 <div class= "section container" id="envision">
     <div class = "row">
-        <div class = "col s12">
+        <div class = "col s4">
+          <img src="">
+        </div>
+        <div class = "col s8">
             <h1> What We Envision</h1>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </div>
     </div>
 </div>
+
 
 <?php
     include 'footer.php';
