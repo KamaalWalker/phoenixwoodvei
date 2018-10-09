692656cf094becffcfd7f102b2c0fa32b214e892
JeyrikPaduga <35312105+JeyrikPaduga@users.noreply.github.com>
2018-10-09 13:34:35 -0700

    Update about.php

--- phoenixwoodvei.com/about.php
+++ phoenixwoodvei.com/about.php
@@ -8,26 +8,30 @@
     <iframe width="900" height="560" src="https://www.youtube.com/embed/PdRcFpGZuVY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 </div>
 
-<div class= "section container" id="whoweare"> 
-    <div class="row"> 
-        <div class="col s8">
-            <h1> Who We Are</h1>
-            Lorem ipsum dolor sit amet,  consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </div>
-        <div class="col s4">
-            <img src="">
-        </div>
-    </div>
-</div>
-
 <div class= "section container" id="envision">
     <div class = "row">
         <div class = "col s12">
-            <h1> What We Envision</h1>
+            <h1> Who We Are</h1>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </div>
     </div>
 </div>
+
+<div class="section parallax-container" id="landing2">
+    <div class="parallax">
+        <div class="col s7">
+             <img src="images/forest-fire1.jpg">
+        </div>
+      <div class="infoblock left" data-aos="fade-right">
+        <div class="row">
+          <div class="col s5">
+              <h1> What We Envision </h1>
+            Lorem ipsum dolor sit amet,  consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
 <?php
     include 'footer.php';
