9f41591ac622657242ceb2bfebc96d1641014db2
xDarling <43828133+xDarling@users.noreply.github.com>
2018-10-08 21:33:15 -0700

    Update store.php
    Overhauled store system to staggered items with prospective products, info, and category
    
    No, there is no mobile integration as of yet

--- phoenixwoodvei.com/store.php
+++ phoenixwoodvei.com/store.php
@@ -6,37 +6,54 @@
 
 <div class="section container">
   <div class="row valign-wrapper">
-    <div class="col s6 red">
-      <div class="hover-hide center">
+    <div class="col s6 center">
+      <a href="#">
+      <h4>Furniture</h4>
+      <div class="hover-hide">
+        <br>
         <i class="material-icons small">unfold_more</i>
         <br>See More
+        <p>+10 Products</p>
       </div>
+      </a>
     </div>
-    <div class="col s3 ">
-      <div class="hover-hide center">
+    <div class="col s3 center">
+      <a href="#">
+      <img src="images/PW-logo.png">
+      <div class="hover-hide">
         <p>Product</p>
         <p class="green-text">$55</p>
+        <p class="more">See More</p>
       </div>
+      </a>
     </div>
-    <div class="col s3 ">
-      <div class="hover-hide center">
+    <div class="col s3 center">
+      <a href="#">
+      <img src="images/PW-logo.png">
+      <div class="hover-hide">
         <p>Product</p>
         <p class="green-text">$55</p>
+        <p class="more">See More</p>
       </div>
+      </a>
     </div>
   </div>
   
   <div class="row valign-wrapper">
-    <div class="col s3 ">
-      <div class="hover-hide center">
+    <div class="col s3 center">
+      <a href="#">
+      <img src="images/PW-logo.png">
+      <div class="hover-hide">
         <p>Product</p>
         <p class="green-text">$55</p>
+        <p class="more">See More</p>
       </div>
+      </a>
     </div>
     <div class="col s9 ">
       <div class="center">
-        <i class="material-icons small">info</i>
-        <br>Our Products
+        <h5>Explore</h5>
+        <p class="grey-text">Sed varius ultrices accumsan. Phasellus luctus efficitur arcu, at gravida justo pulvinar in. Vivamus ac lacinia augue. efficitur arcu, at gravida justo pulvinar in. Vivamus ac lacinia augue.</p>
       </div>
     </div>
   </div>
@@ -46,81 +63,117 @@
   <div class="row valign-wrapper">
     <div class="col s6 ">
       <div class="center">
-        <i class="material-icons small">info</i>
-        <br>Our Products
+        <h5>Reclaim</h5>
+        <p class="grey-text">Sed varius ultrices accumsan. Phasellus luctus efficitur arcu, at gravida justo pulvinar in.</p>
       </div>
     </div>
-    <div class="col s6 red">
-      <div class="hover-hide center">
+    <div class="col s6 center">
+      <a href="#">
+      <h4>Accessories</h4>
+      <div class="hover-hide">
+        <br>
         <i class="material-icons small">unfold_more</i>
         <br>See More
+        <p>+10 Products</p>
       </div>
+      </a>
     </div>
   </div>
   <div class="row valign-wrapper">
-    <div class="col s3 ">
-      <div class="hover-hide center">
+    <div class="col s3 center">
+      <a href="#">
+      <img src="images/accessory_1.png">
+      <div class="hover-hide">
         <p>Product</p>
         <p class="green-text">$55</p>
+        <p class="more">See More</p>
       </div>
+      </a>
     </div>
-    <div class="col s3 ">
-      <div class="hover-hide center">
+    <div class="col s3 center">
+      <a href="#">
+      <img src="images/accessory_2.png">
+      <div class="hover-hide">
         <p>Product</p>
         <p class="green-text">$55</p>
+        <p class="more">See More</p>
       </div>
+      </a>
     </div>
-    <div class="col s3 ">
-      <div class="hover-hide center">
+    <div class="col s3 center">
+      <a href="#">
+      <img src="images/PW-logo.png">
+      <div class="hover-hide">
         <p>Product</p>
         <p class="green-text">$55</p>
+        <p class="more">See More</p>
       </div>
+      </a>
     </div>
-    <div class="col s3 amber lighten-2">
-      <div class="hover-hide center">
-        <p>Popular Product</p>
-        <p class="green-text">$25</p>
-        <i class="material-icons small">whatshot</i>
+    <div class="col s3 amber lighten-2 center">
+      <a href="#">
+      <img src="images/accessory_3.png">
+      <div class="hover-hide">
+        <p>Product</p>
+        <p class="green-text">$55</p>
+        <p class="more">See More</p>
       </div>
+      </a>
     </div>
   </div>
 </div>
 
 <div class="section container">
   <div class="row valign-wrapper">
-    <div class="col s6 red">
-      <div class="hover-hide center">
+    <div class="col s6 center">
+      <a href="#">
+      <h4>Recreational</h4>
+      <div class="hover-hide">
+        <br>
         <i class="material-icons small">unfold_more</i>
         <br>See More
+        <p>+10 Products</p>
       </div>
+      </a>
     </div>
-    <div class="col s3 amber lighten-2">
-      <div class="hover-hide center">
-        <p>Popular Product</p>
-        <p class="green-text">$105</p>
-        <i class="material-icons small">whatshot</i>
-      </div>
-    </div>
-    <div class="col s3 ">
-      <div class="hover-hide center">
+    <div class="col s3 amber lighten-2 center">
+      <a href="#">
+      <img src="images/PW-logo.png">
+      <div class="hover-hide">
         <p>Product</p>
         <p class="green-text">$55</p>
+        <p class="more">See More</p>
       </div>
+      </a>
+    </div>
+    <div class="col s3 center">
+      <a href="#">
+      <img src="images/PW-logo.png">
+      <div class="hover-hide">
+        <p>Product</p>
+        <p class="green-text">$55</p>
+        <p class="more">See More</p>
+      </div>
+      </a>
     </div>
   </div>
   
   <div class="row valign-wrapper">
     <div class="col s9 ">
       <div class="center">
-        <i class="material-icons small">info</i>
-        <br>Our Products
+        <h5>Create</h5>
+        <p class="grey-text">Sed varius ultrices accumsan. Phasellus luctus efficitur arcu, at gravida justo pulvinar in. Vivamus ac lacinia augue. efficitur arcu, at gravida justo pulvinar in. Vivamus ac lacinia augue.</p>
       </div>
     </div>
-    <div class="col s3 ">
-      <div class="hover-hide center">
+    <div class="col s3 center">
+      <a href="#">
+      <img src="images/PW-logo.png">
+      <div class="hover-hide">
         <p>Product</p>
         <p class="green-text">$55</p>
+        <p class="more">See More</p>
       </div>
+      </a>
     </div>
   </div>
 </div>
