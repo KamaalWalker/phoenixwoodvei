be0d5a1480ad346380d79011fc0f7316c005a4a6
KamaalWalker <32143331+KamaalWalker@users.noreply.github.com>
2018-10-18 13:40:04 -0700

    Update about1.php

--- phoenixwoodvei.com/about1.php
+++ phoenixwoodvei.com/about1.php
@@ -80,7 +80,8 @@ h3 {
 
 <div style="color: #777;background-color:white;text-align:center;padding:50px 80px;text-align: justify;">
   <h3 style="text-align:center;">Who We Are</h3>
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus maximus tristique purus et porttitor. Morbi ac nisl velit. Praesent pellentesque, est sed pellentesque laoreet, mi dolor finibus magna, eget bibendum leo ipsum quis ligula. Donec aliquet enim urna, sed rutrum neque efficitur non. Curabitur suscipit lacus malesuada volutpat maximus. Duis lectus risus, dictum vitae consectetur quis, facilisis eget libero. Aliquam consequat bibendum enim, at rhoncus arcu eleifend a. Fusce sapien risus, ultricies imperdiet condimentum vitae, molestie quis augue. Nunc sit amet malesuada elit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas imperdiet vitae orci sit amet euismod.</p>
+  <p>Phoenix Wood is a company focused on the preservation of the forests through the process of recycling wood killed by bark beetles into natural products. We are dedicated making our forest and the community a healthier 
+     and more efficient place through the use of nature's resources by clearing the forests of infested and dead wood.</p>
 </div>
 
 <div class="bgimg-2">
