57942459cea19736321354dffb15528f21e8a27b
KamaalWalker <32143331+KamaalWalker@users.noreply.github.com>
2018-10-18 18:25:55 -0700

    Updated "Who we are"

--- phoenixwoodvei.com/about.php
+++ phoenixwoodvei.com/about.php
@@ -2,32 +2,129 @@
     include 'header.php';
 ?>
 
-<link href="css/about.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-
-<div class= "section container" id="video">
-    <iframe width="900" height="560" src="https://www.youtube.com/embed/PdRcFpGZuVY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+<link href="css/about.css" rel="stylesheet">
+  
+<div class="parallax-container center" id="bgimg-1">
+  <div class="parallax"></div>
+  <div class="block">
+    <h5>About Us</h5>
+  </div>
 </div>
 
-<div class= "section container" id="whoweare"> 
-    <div class="row"> 
-        <div class="col s8">
-            <h1> Who We Are</h1>
-            Lorem ipsum dolor sit amet,  consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </div>
-        <div class="col s4">
-            <img src="">
-        </div>
+<div class="container section">
+  <div class="row">
+    <div class="col s12">
+      <h3>Who We Are</h3>
+      <p>Phoenix Wood is a company focused on the preservation of our forests through the process of recycling wood killed by bark beetles into natural products. By removing this wood we relieve the strain put on the forest resources, reduce the fuel available for wildfires, and reduce the carbon emissions that would have been released by that dead tree.</p>
     </div>
+  </div>
 </div>
 
-<div class= "section container" id="envision">
-    <div class = "row">
-        <div class = "col s12">
-            <h1> What We Envision</h1>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </div>
-    </div>
+<div class="parallax-container center" id="bgimg-2">
+  <div class="parallax"></div>
+  <div class="quoteblock">
+    <h5>ed rutrum neque efficitur non. Curabitur suscipit lacus malesuada volutpat maximus. Duis lectus risus, dictum vitae consectetur quis, facilisis eget libero. Aliquam consequat bibendum enim,</h5>
+  </div>
 </div>
+
+<div class="parallax-container center" id="bgimg-3">
+  <div class="parallax"></div>
+  <div class="quoteblock">
+    <h5>ed rutrum neque efficitur non. Curabitur suscipit lacus malesuada volutpat maximus. Duis lectus risus, dictum vitae consectetur quis, facilisis eget libero. Aliquam consequat bibendum enim,</h5>
+  </div>
+</div>
+
+<div class="container section">
+  <div class="row">
+    <div class="col s12">
+      <h3>What We Envision</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus maximus tristique purus et porttitor. Morbi ac nisl velit. Praesent pellentesque, est sed pellentesque laoreet, mi dolor finibus magna, eget bibendum leo ipsum quis ligula. Donec aliquet enim urna, sed rutrum neque efficitur non. Curabitur suscipit lacus malesuada volutpat maximus. Duis lectus risus, dictum vitae consectetur quis, facilisis eget libero. Aliquam consequat bibendum enim, at rhoncus arcu eleifend a. Fusce sapien risus, ultricies imperdiet condimentum vitae, molestie quis augue. Nunc sit amet malesuada elit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas imperdiet vitae orci sit amet euismod.</p>
+    </div>
+  </div>
+</div>
+
+<div class="parallax-container center" id="bgimg-4">
+  <div class="parallax"></div>
+</div>
+
+<div class="container section" id="cards">
+  <div class="row">
+    <div class="col s12">
+      <h3>Our Team</h3>
+    </div>
+  </div>
+  <div class="row">
+    <a href="#">
+    <div class="col s4">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/default.png">
+        </div>
+        <div class="card-content">
+          <h5>Andrew Chaffee</h5>
+          <p>CEO</p>
+        </div>
+      </div>
+    </div>
+    </a>
+    <a href="#">
+    <div class="col s4">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/default.png">
+        </div>
+        <div class="card-content">
+          <h5>Samir Mody</h5>
+          <p>CEO</p>
+        </div>
+      </div>
+    </div>
+    </a>
+    <a href="#">
+    <div class="col s4">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/default.png">
+        </div>
+        <div class="card-content">
+          <h5>Gurman Dhaliwal</h5>
+          <p>CEO</p>
+        </div>
+      </div>
+    </div>
+    </a>
+  </div>
+  <div class="row">
+    <a href="#">
+    <div class="col s4">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/default.png">
+        </div>
+        <div class="card-content">
+          <h5>Christian Carrido</h5>
+          <p>CTO</p>
+        </div>
+      </div>
+    </div>
+    </a>
+    <a href="#">
+    <div class="col s4">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/default.png">
+        </div>
+        <div class="card-content">
+          <h5>Gage Green</h5>
+          <p>CVCO</p>
+        </div>
+      </div>
+    </div>
+    </a>
+  </div>
+  <a href="staff-directory.php">Staff Directory</a>
+</div>
+
 
 <?php
     include 'footer.php';
